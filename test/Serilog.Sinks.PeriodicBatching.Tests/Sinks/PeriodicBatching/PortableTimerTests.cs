e9fdc2ddcdee89f1d8706741ba4e8b5b20f1082d
Sergey Komisarchik <komisarchik@media-saturn.com>
2016-09-14 23:49:54 +0300

    [PortableTimer] tests

--- test/Serilog.Sinks.PeriodicBatching.Tests/Sinks/PeriodicBatching/PortableTimerTests.cs
+++ test/Serilog.Sinks.PeriodicBatching.Tests/Sinks/PeriodicBatching/PortableTimerTests.cs
@@ -1,9 +1,8 @@
-﻿#if !WAITABLE_TIMER
-
-using Serilog.Debugging;
+﻿using Serilog.Debugging;
 using Serilog.Sinks.PeriodicBatching;
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Serilog.Tests.Sinks.PeriodicBatching
@@ -33,13 +32,35 @@ namespace Serilog.Tests.Sinks.PeriodicBatching
 
             using (var timer = new PortableTimer(delegate { Thread.Sleep(50); wasCalled = true; }))
             {
-                timer.Start(TimeSpan.FromMilliseconds(10));
+                timer.Start(TimeSpan.FromMilliseconds(20));
             }
 
             Thread.Sleep(100);
 
             Assert.False(wasCalled, "tick handler was called");
             Assert.False(writtenToSelflog, "message was written to SelfLog");
+        }
+
+        [Fact]
+        public void WhenActiveShouldCancel_OnDispose()
+        {
+            bool wasCalled = false;
+            bool writtenToSelflog = false;
+
+            SelfLog.Enable(_ => writtenToSelflog = true);
+
+            using (var timer = new PortableTimer(
+                                    token => {
+                                        wasCalled = true;
+                                        Task.Delay(TimeSpan.FromMilliseconds(50), token).GetAwaiter().GetResult();
+                                    }))
+            {
+                timer.Start(TimeSpan.FromMilliseconds(20));
+                Thread.Sleep(40);
+            }
+
+            Assert.True(wasCalled, "tick handler wasn't called");
+            Assert.True(writtenToSelflog, "message wasn't written to SelfLog");
         }
 
         [Fact]
@@ -53,7 +74,51 @@ namespace Serilog.Tests.Sinks.PeriodicBatching
             Assert.False(wasCalled);
             Assert.Throws<ObjectDisposedException>(() => timer.Start(TimeSpan.Zero));
         }
+
+        [Fact]
+        public void WhenOverlapsShouldProcessOneAtTime_OnTick()
+        {
+            bool userHandlerOverlapped = false;
+
+            PortableTimer timer = null;
+            timer = new PortableTimer(
+                _ =>
+                {
+                    if (Monitor.TryEnter(timer))
+                    {
+                        try
+                        {
+                            timer.Start(TimeSpan.FromMilliseconds(0));
+                            Thread.Sleep(1);
+                        }
+                        finally
+                        {
+                            Monitor.Exit(timer);
+                        }
+                    }
+                    else
+                    {
+                        userHandlerOverlapped = true;
+                    }                    
+                });
+
+            timer.Start(TimeSpan.FromMilliseconds(1));
+            Thread.Sleep(50);
+            timer.Dispose();
+
+            Assert.False(userHandlerOverlapped);
+        }
+
+        [Fact]
+        public void CanBeDisposedFromMultipleThreads()
+        {
+            PortableTimer timer = null;
+            timer = new PortableTimer(_ => timer.Start(TimeSpan.FromMilliseconds(1)));
+
+            timer.Start(TimeSpan.Zero);
+            Thread.Sleep(50);
+
+            Parallel.For(0, Environment.ProcessorCount * 2, _ => timer.Dispose());
+        }        
     }
 }
-
-#endif
